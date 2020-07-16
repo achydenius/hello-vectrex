1118cbf0992ba0be8f5af66801b1426118b21a3a
Arto Chydenius <arto.chydenius@futurice.com>
2020-07-16 14:49:44 +0300

    Draw and animate dots

--- main.asm
+++ main.asm
@@ -1,6 +1,7 @@
-Music           EQU     $FD0D
-Wait_Recal	EQU	$F192
+                INCLUDE "vectrex.inc"
 
+Music           EQU     $FD0D
+Star_Count      EQU     8
 
                 ORG     0
 
@@ -11,5 +12,35 @@ Wait_Recal	EQU	$F192
                 FCC     "HELLO.BIN"
                 FCB     $80,0
 
-main		jsr     Wait_Recal
+                ldx     #coords
+                ldb     #Star_Count
+randomize       jsr     Random_3
+                sta     ,x+
+                jsr     Random_3
+                sta     ,x+
+                decb
+                bne     randomize
+
+main            jsr     Wait_Recal
+                lda     #$80
+                sta     VIA_t1_cnt_lo
+
+                jsr     Intensity_5F
+                ldx     #coords
+loop            ldd     ,x++
+                subb    offset
+                jsr     Moveto_d
+                jsr     Dot_here
+                jsr     Reset0Int
+
+                cmpx    #coords+(Star_Count*2)
+                bne     loop
+
+                inc     offset
+
 		bra	main
+
+                ORG     $C880
+
+coords          RMD     Star_Count
+offset          RMB     1
