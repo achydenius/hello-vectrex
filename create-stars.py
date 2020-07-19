07188db3a171e8a66df523b4f8ea1685b857994d
Arto Chydenius <arto.chydenius@futurice.com>
2020-07-19 12:57:47 +0300

    Animate multiple layers of dots

---
+++ create-stars.py
@@ -0,0 +1,24 @@
+from random import seed, randint
+import sys
+
+RANDOM_SEED = 0
+STAR_COUNT = 8
+LAYER_COUNT = 3
+MIN = -128
+MAX = 127
+
+seed(RANDOM_SEED)
+
+# Randomize lists of stars with distinct x values
+layers = []
+for i in range(0, LAYER_COUNT):
+    stars = {}
+    while len(stars) < STAR_COUNT:
+        stars[randint(MIN, MAX)] = randint(MIN, MAX)
+
+    layers.append(stars)
+
+for layer in layers:
+    sys.stdout.write(
+        ",".join("{},{}".format(y, x) for x, y in layer.items()) + "\n"
+    )
--- main.asm
+++ main.asm
@@ -1,6 +1,7 @@
                 INCLUDE "vectrex.inc"
 
 Music           EQU     $FD0D
+Layer_Count     EQU     3
 Star_Count      EQU     8
 
                 ORG     0
@@ -12,35 +13,42 @@ Star_Count      EQU     8
                 FCC     "HELLO.BIN"
                 FCB     $80,0
 
-                ldx     #coords
-                ldb     #Star_Count
-randomize       jsr     Random_3
-                sta     ,x+
-                jsr     Random_3
-                sta     ,x+
-                decb
-                bne     randomize
-
 main            jsr     Wait_Recal
                 lda     #$80
                 sta     VIA_t1_cnt_lo
 
                 jsr     Intensity_5F
                 ldx     #coords
-loop            ldd     ,x++
-                subb    offset
+                ldy     #offsets
+
+                lda     #Layer_Count
+                sta     i
+layer           lda     #Star_Count
+                sta     j
+star            ldd     ,x++            ; Draw offsetted star
+                addb    ,y
                 jsr     Moveto_d
                 jsr     Dot_here
                 jsr     Reset0Int
 
-                cmpx    #coords+(Star_Count*2)
-                bne     loop
+                dec     j
+                bne     star
 
-                inc     offset
+                lda     ,y              ; Subtract offset by layer counter
+                suba    i
+                sta     ,y+
+
+                dec     i
+                bne     layer
 
 		bra	main
 
+coords          FCB     69,87,-108,4,120,79,27,116,55,-17,-57,16,-57,-80,0,-53
+                FCB     30,-78,-91,41,113,-77,53,94,33,-24,116,98,5,-97,-121,-81
+                FCB     76,-128,124,42,-4,38,-96,-31,-15,-6,-56,101,-82,-87,35,122
+
                 ORG     $C880
 
-coords          RMD     Star_Count
-offset          RMB     1
+i               RMB     1               ; Layer counter
+j               RMB     1               ; Star counter
+offsets         RMB     Layer_Count     ; Star layer offset
